cf9ee8ef92a7c127e2460ccc66c8a5e1d4c4bf20
zhp <zhp1223912573@qq.com>
2022-09-07 14:45:17 +0800

    刷题记录

--- src/递归/最佳买卖股票时机含冷冻期_lc_309.java
+++ src/递归/最佳买卖股票时机含冷冻期_lc_309.java
@@ -16,11 +16,29 @@ package 递归;
  *
  * 《状态推导》
  * 0：之前就持有股票则dp[i][0]=dp[i-1][0]
- *      或者，昨天是冷冻期，今天刚好买入股票dp[i][0] = dp[i][3]-prices[i]
- *      亦或者，昨天不是冷冻期，今天刚好买入股票dp[i][0] = dp[i][2]-prices[i]
+ *      或者，昨天是冷冻期，今天刚好买入股票dp[i][0] = dp[i-1][3]-prices[i]
+ *      亦或者，昨天不是冷冻期，今天刚好买入股票dp[i][0] = dp[i-1][1]-prices[i]
  *      取三者最大值
- * 1：
+ * 1：前一天就不持有股票dp[i][1] = dp[i-1][1]
+ *    前一天是冷冻期dp[i][1] = dp[i-1][2]
+ * 2: 昨天是买入股票状态dp[i][2] = dp[i-1][0]+prices[i]
+ * 3.昨天卖出股票dp[i][3] = dp[i-1][2]
  *
  */
 public class 最佳买卖股票时机含冷冻期_lc_309 {
+    public int maxProfit(int[] prices) {
+        if(prices==null||prices.length<=1) return 0;
+        int len = prices.length;
+        int dp[][] = new int[len][4];
+
+        dp[0][0] = -prices[0];
+
+        for(int i=1;i<len;i++){
+            dp[i][0] = Math.max(dp[i-1][0],Math.max(dp[i-1][3]-prices[i],dp[i-1][1]-prices[i]));
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][2]);
+            dp[i][2] =  dp[i-1][0]+prices[i];
+            dp[i][3] = dp[i-1][2];
+        }
+        return Math.max(dp[len-1][3],Math.max(dp[len-1][2],dp[len-1][1]));
+    }
 }
