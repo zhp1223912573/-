2b085abb2ba460eb69ee39c96def3380ec85c2cb
zhp <zhp1223912573@qq.com>
2022-09-07 14:49:14 +0800

    刷题记录

--- src/递归/卖股票的最佳时期IV_lc_188.java
+++ src/递归/卖股票的最佳时期IV_lc_188.java
@@ -30,9 +30,9 @@ public class 卖股票的最佳时期IV_lc_188 {
         }
 
         for(int i=1;i<len;i++){
-            for(int j=1;j<2*k;j++){
-                dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-1]-prices[i]);
-                dp[i][j+1] = Math.max(dp[i-1][j+1],dp[i-1][j-1]+prices[i]);
+            for (int j = 0; j < 2 * k - 1; j += 2) {
+                dp[i][j + 1] = Math.max(dp[i - 1][j + 1], dp[i - 1][j] - prices[i]);
+                dp[i][j + 2] = Math.max(dp[i - 1][j + 2], dp[i - 1][j + 1] + prices[i]);
             }
         }
         return dp[len-1][2*k];
