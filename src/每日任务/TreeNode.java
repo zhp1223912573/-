53d1263be8ea10a7907eb9bf9e64d4bb71b7ad3d
zhp <zhp1223912573@qq.com>
2022-10-22 12:26:47 +0800

    10.22

---
+++ src/每日任务/TreeNode.java
@@ -0,0 +1,12 @@
+package 每日任务;
+
+/**
+ * @author zhp
+ * @date 2022-10-22 10:56
+ */
+public class TreeNode {
+      int val;
+     TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
---
+++ src/每日任务/offer_64_求1_n和.java
@@ -0,0 +1,19 @@
+package 每日任务;
+
+/**
+ * @author zhp
+ * @date 2022-10-22 10:44
+ * https://leetcode.cn/problems/qiu-12n-lcof/submissions/
+ */
+public class offer_64_求1_n和 {
+    /**
+     * 要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句
+     * 那就只能在递归中完成计算，并利用短路运算符&&
+     * @param n
+     * @return
+     */
+    public int sumNums(int n) {
+        boolean flag = n>1&& (n+=sumNums(n-1))>0;
+        return n;
+    }
+}
