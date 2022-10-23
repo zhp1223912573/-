ef2475bbf4a45ce21c6c5975385729cfc1510ce2
zhp <zhp1223912573@qq.com>
2022-10-23 12:22:07 +0800

    10.23

---
+++ src/每日任务/ms05_04_下一个数.java
@@ -0,0 +1,48 @@
+package 每日任务;
+
+/**
+ * @author zhp
+ * @date 2022-10-23 12:31
+ * https://leetcode.cn/problems/closed-number-lcci/comments/
+ */
+public class ms05_04_下一个数 {
+    /**
+     * 找到两个和当前数值二进制表示时1的个数一直，且最接近的两个数
+     */
+
+    /**
+     * 暴力枚举
+     * 以当前数为起点，分别向上向下求解。
+     */
+    public int[] findClosedNumbers(int num) {
+        int up = num + 1;//向上枚举
+        int down = num - 1;//向下枚举
+        int count = findOneCount(num);//num的1的个数
+        while (findOneCount(up) != count) {
+            up++;
+            if (up < 0) {//越界了那就是找不到，设置为-1
+                up = -1;
+                break;
+            }
+        }
+        while (findOneCount(down) != count) {
+            down--;
+            if (down < 0) {//变为负数了那就是找不到了，设置为-1
+                down = -1;
+                break;
+            }
+        }
+        return new int[]{up, down};
+    }
+    //求数的二进制1的个数
+    private static int findOneCount(int num) {
+        int count = 0;
+        while (num != 0) {
+            num &= num - 1;
+            count++;
+        }
+        return count;
+    }
+
+
+}
---
+++ src/每日任务/ms05_06.java
@@ -0,0 +1,27 @@
+package 每日任务;
+
+/**
+ * @author zhp
+ * @date 2022-10-23 12:25
+ * https://leetcode.cn/problems/convert-integer-lcci/submissions/
+ */
+public class ms05_06 {
+
+    /**
+     * 计算两数需要变化多少位才能相等
+     * 异或，计算1的个数即可
+     * @param A
+     * @param B
+     * @return
+     */
+    public int convertInteger(int A, int B) {
+        int xor = A^B;
+        int cnt = 0;
+        //这里用来一个小技巧，一个数与自己-1相与，直到自己为0，那么循环的次数就是1的个数
+        while(xor!=0){
+            xor&=(xor-1);
+            cnt++;
+        }
+        return cnt;
+    }
+}
